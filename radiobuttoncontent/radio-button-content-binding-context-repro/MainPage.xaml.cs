09540f2009748958325d78cf985dca1d35b6ed28
Sam Jordan <sjordan@gsspos.co.uk>
2022-02-22 09:26:47 +0000

    Add project files.

---
+++ radiobuttoncontent/radio-button-content-binding-context-repro/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace radio_button_content_binding_context_repro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ radiobuttoncontent/radio-button-content-binding-context-repro/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace radio_button_content_binding_context_repro
+{
+    internal class MainPageViewModel
+    {
+        public string Text { get; set; } = "This text came from the view model";
+    }
+}
